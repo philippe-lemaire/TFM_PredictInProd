52b9d2bf8dbdbbc1d8dec8964e6e0ab2b0920dbb
Philippe Lemaire <philippe.lemaire@protonmail.com>
2021-06-24 14:35:21 +0200

    ready for docker locally

--- api/fast.py
+++ api/fast.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import pandas as pd
+from datetime import datetime
+import pytz
+import joblib
 
 
 app = FastAPI()
@@ -27,11 +31,47 @@ def pred(
     dropoff_latitude,
     passenger_count,
 ):
-    return {
-        "pickup_datetime": pickup_datetime,
-        "pickup_longitude": pickup_longitude,
-        "pickup_latitude": pickup_latitude,
-        "dropoff_longitude": dropoff_longitude,
-        "dropoff_latitude": dropoff_latitude,
-        "passenger_count": passenger_count,
+
+    pickup_datetime = datetime.strptime(pickup_datetime, "%Y-%m-%d %H:%M:%S")
+
+    # localize the user datetime with NYC timezone
+    eastern = pytz.timezone("US/Eastern")
+    localized_pickup_datetime = eastern.localize(pickup_datetime, is_dst=None)
+    utc_pickup_datetime = localized_pickup_datetime.astimezone(pytz.utc)
+    formatted_pickup_datetime = utc_pickup_datetime.strftime("%Y-%m-%d %H:%M:%S UTC")
+
+    request_params = {
+        "key": [0],
+        "pickup_datetime": ([formatted_pickup_datetime]),
+        "pickup_longitude": [float(pickup_longitude)],
+        "pickup_latitude": [float(pickup_latitude)],
+        "dropoff_longitude": [float(dropoff_longitude)],
+        "dropoff_latitude": [float(dropoff_latitude)],
+        "passenger_count": [int(passenger_count)],
     }
+
+    X_pred = pd.DataFrame(request_params)
+    pipeline = joblib.load("model.joblib")
+    pred = pipeline.predict(X_pred)
+
+    return {"prediction": pred[0]}
+
+
+if __name__ == "__main__":
+
+    pickup_datetime = "2013-07-06 17:18:00"
+    pickup_longitude = "-73.950655"
+    pickup_latitude = "40.783282"
+    dropoff_longitude = "-73.984365"
+    dropoff_latitude = "40.769802"
+    passenger_count = "1"
+
+    pred = pred(
+        pickup_datetime,
+        pickup_longitude,
+        pickup_latitude,
+        dropoff_longitude,
+        dropoff_latitude,
+        passenger_count,
+    )
+    print(pred)
